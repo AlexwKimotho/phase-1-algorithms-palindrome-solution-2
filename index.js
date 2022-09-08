511e723bf622659a80285939a2ec96564dcb1cb5
Alex_Wachira_Kimotho <alex.wachira@student.moringashool.com>
2022-09-08 08:10:51 +0300

    Done

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  //iterate from the beginning to the middle
+  for (let i = 0; i < word.length/ 2; i++){
+    //check if first and last word is the same
+    if (word[i] !== word[word.length - 1 - i]) {
+      return false;
+  }
+  }
+  return true;
 }
 
 /* 
   Add your pseudocode here
+  if word === reverse.word => true { (is palindrome)}
+  if word  === reverse.word => false{{is not palindrome}}
+  convert word to array
+  reverse array values
+  convert reversed array to Word
+  compare word with reversedWord
 */
 
 /*
   Add written explanation of your solution here
+  function reverseString converts word to array then the array is reversed then turned back to a string(word
+    ) the function is allocated a variable called  reversedword which we compare it with the word to
+    find whether string is palindrome on not. 
 */
 
 // You can run `node index.js` to view these console logs
